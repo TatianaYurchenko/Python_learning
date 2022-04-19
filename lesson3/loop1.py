2c4c83c1bdf0393a4e48352ba27e71ff3dacdd97
Tatiana Yurchenko <taf88156@mail.ru>
2022-04-19 11:51:51 +0300

    1-я часть дз по циклам

---
+++ lesson3/loop1.py
@@ -0,0 +1,34 @@
+# y = 100
+# for x in range(6):
+#     if x == 3:
+#         print(x, y)
+#     y+=1
+# ____________________
+# y = "Hello Python"
+# a = y.split(" ")
+# print(a)
+# for i in a:
+#     print(i)
+
+# y = "Hello Python!"
+# for i in y:
+#     print(i)
+#
+# _____________
+
+# i = 1
+# while i < 100:
+#     print(i)
+#     if i == 45:
+#         break
+#     i += 1
+# ______________
+
+# for x in range(1, 30, 2):
+#     print(x)
+# ____________
+
+# for x in range(2, 21, 2):
+#         print(x)
+# _______________
+
--- str1.py
+++ str1.py
@@ -1,4 +1,9 @@
-milk = 10
-eggs = 12
-text = f'Please don\'t forget to buy {milk**2} gallons of milk and {eggs} eggs'
-print(text)
+# milk = 10
+# eggs = 12
+# text = f'Please don\'t forget to buy {milk**2} gallons of milk and {eggs} eggs'
+# print(text)
+# _______________
+
+a = "желтые груши"
+c = sorted(a, reverse = True)
+print(c)
