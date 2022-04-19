decf8453e805f31a614a167d05e7523dc33ef76c
Tatiana Yurchenko <taf88156@mail.ru>
2022-04-19 15:27:20 +0300

    программы к уроку по функциям

---
+++ lesson3/function_1.py
@@ -0,0 +1,32 @@
+# def sum_f(x, y):
+#     res = x + y
+#     return res
+# a = 12
+# b = 13
+# v = sum_f(a,b)
+# print(f'result:{v}')
+#_______________
+# сравнение
+# def compare_fun(x,y):
+#     if x > y:
+#         return True
+#     else:
+#         return False
+# print(compare_fun(10,5))
+#___________
+# глобальные переменные Переменная перезаписывается
+
+#x = 45
+
+# def dummy_function():
+#     global x
+#     x = 12
+#     x += 1
+#     print(x)
+#
+# print(x)
+# dummy_function()
+# print(x)
+
+#___________
+
