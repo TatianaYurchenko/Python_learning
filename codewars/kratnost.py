352b803baf804ff1c3eb7d24daf17bda659c74bd
Tatiana Yurchenko <taf88156@mail.ru>
2022-04-20 17:54:07 +0300

    добавлены еще 2 программы для нахождения кратности

--- codewars/kratnost.py
+++ codewars/kratnost.py
@@ -5,12 +5,18 @@
 #     if i % a == 0:
 #         c.append(i)
 # print(c)
+# c = []
+# def kratnost_chisel(a, b):
+#
+#     for i in range(1, b + 1):
+#         if i % a == 0:
+#            c.append(i)
+#     # print(c)
+#     return c
+# print(kratnost_chisel(2,12))
+#
 
-def kratnost_chisel(a, b):
-    c = []
-    for i in range(1, b + 1):
-        if i % a == 0:
-           c.append(i)
-    print(c)
-    return c
-kratnost_chisel(2,12)
+def find_multiples(i, l):
+    return list(range(i, l+1, i))
+
+print(find_multiples(2,10))
