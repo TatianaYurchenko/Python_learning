51727a02eeaaf175fdaef580496b189e281c4e6d
Tatiana Yurchenko <taf88156@mail.ru>
2023-09-06 11:51:42 +0300

    add new dir and examles of output

---
+++ Vivod/Oformlenie.py
@@ -0,0 +1,6 @@
+print(f'{float_number:.3f} выдает три точки после запятой')
+# ф-я добавляет нолики
+print(f'текущее время: {hour:0>2}:{min_min:0>2}
+
+txt1 = "My name is {fname}, I'm {age}".format(fname = "John", age = 36)
+txt3 = "My name is {}, I'm {}".format("John",36)
