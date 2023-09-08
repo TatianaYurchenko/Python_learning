2503260df859415e34211670572f5bd3b3cb8f39
Tatiana Yurchenko <taf88156@mail.ru>
2023-09-08 17:43:45 +0300

    'Заполнение матрицы 3x2 нулями'

---
+++ Spisky/vloz_spisky.py
@@ -0,0 +1,9 @@
+# # Заполнение матрицы 3x2 нулями
+# a = []
+# n = 3 #int(input()) #stroka
+# m = 2 #int(input()) #stolb
+# for i in range(n):
+#     a.append([0]*m)
+# for i in a:
+#     print(i)
+
--- Vivod/Oformlenie.py
+++ Vivod/Oformlenie.py
@@ -1,12 +1,19 @@
 # использование рандомных чисел
 from random import randint
+# список цифр букв и символов
+# from string import printable
+# print(printable)
+
 # for i in range(5):
 #  a = randint(1,100)
 # print(a)
 
+# кодировка ascii
+# (i, ord(i))
 # вывод в одну строчку print(a, end' ')
 
 # print(f'{float_number:.3f} выдает три точки после запятой')
+
 # # ф-я добавляет нолики
 # print(f'текущее время: {hour:0>2}:{min_min:0>2}
 #
@@ -14,5 +21,11 @@ from random import randint
 # txt3 = "My name is {}, I'm {}".format("John",36)
 #
 # n, m = list(map(int, input().split()))
-
+# vowels = "aeiouy"
 # a,b,c = range(5,8)
+# #  отсечение спец символов
+# s = 'jhkjhkh34'
+# for i in s.lower():
+#     if i >= 'a' and i <= 'z':
+#         print(i)
+
