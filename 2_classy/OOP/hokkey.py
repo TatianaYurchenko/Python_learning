c36bbeca6845d768b72a81e86722d691f1872fb7
Tatiana Yurchenko <taf88156@mail.ru>
2023-10-03 18:52:03 +0300

    feat: add scripts with class

--- 2_classy/OOP/hokkey.py
+++ 2_classy/OOP/hokkey.py
@@ -1,16 +1,58 @@
-# наследование
-class Animal:
-    def can_run(self):
-        print('i can run')
-class Dogs:
-    def can_burck(self):
-        print('i can burck')
+# class Cat:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def get_info(self):
+#         print(f'cat {self.name}, {self.age} age')
+#
+#     def speak(self, sound):
+#         self.sound = sound
+#         print(f'miu miu {sound}')
+# dimka = Cat("mir", 6)
+# print(dimka.__dict__)
+# dimka.get_info()
+# dimka.speak('miu miu')
 
-class Bigle(Animal, Dogs):
-    def can_sleep(self):
-        print('i can sleep')
-# встроенная ф-ция mro которая показывает иерархию наследования
-print(Bigle.mro())
+# Делегирование
+class Person:
+    def __init__(self, name, lastname):
+        self.name = name
+        self.lastname = lastname
+
+    def can_breathe(self):
+        print('могу дышать')
+
+class Teacher(Person):
+    def __init__(self, name, lastname, age):
+        # self.name = name
+        # self.lastname = lastname
+        super().__init__(name, lastname)
+        self.age = age
+
+    def can_breathe(self):
+        print('учитель дышит')
+        super().can_breathe()
+p2 = Person('Denis', 'Denisov')
+t2 = Teacher("iVAN", "Ivanov", 45)
+print(p2.name, p2.lastname)
+print(t2.name, t2.lastname, t2.age)
+p2.can_breathe()
+t2.can_breathe()
+
+
+# # наследование
+# class Animal:
+#     def can_run(self):
+#         print('i can run')
+# class Dogs:
+#     def can_burck(self):
+#         print('i can burck')
+#
+# class Bigle(Animal, Dogs):
+#     def can_sleep(self):
+#         print('i can sleep')
+# # встроенная ф-ция mro которая показывает иерархию наследования
+# print(Bigle.mro())
 
 
 
